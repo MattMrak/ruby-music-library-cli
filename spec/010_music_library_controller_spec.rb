0878b82bbe04ed7fd96fc7486dca90666c7d7d3d
MattMrak <matthewmrakovcic@gmail.com>
2021-01-04 12:00:50 -0500

    Done.

--- spec/010_music_library_controller_spec.rb
+++ spec/010_music_library_controller_spec.rb
@@ -1,5 +1,75 @@
 require "spec_helper"
 
+class MusicLibraryController
+  def initialize(path = "./db/mp3s")
+    MusicImporter.new(path).import
+  end
+  def call
+    input = gets.strip
+    if input != exit
+      puts "Welcome to your music library!"
+      puts "To list all of your songs, enter 'list songs'."
+      puts "To list all of the artists in your library, enter 'list artists'."
+      puts "To list all of the genres in your library, enter 'list genres'."
+      puts "To list all of the songs by a particular artist, enter 'list artist'."
+      puts "To list all of the songs of a particular genre, enter 'list genre'."
+      puts "To play a song, enter 'play song'."
+      puts "To quit, type 'exit'."
+      puts "What would you like to do?"
+      input = gets.chomp
+      case input
+      when 'list songs'
+        list_songs
+      when 'list artists' 
+        list_artists
+      when 'list genres'
+        list_genres
+      when 'play song'
+        play_song
+      when 'list artist'
+        list_artist
+      when 'list genre'
+        list_genre
+      when 'exit'
+        puts "Goodbye" 
+      else
+        puts "Welcome to your music library!"
+      end
+    end
+  end
+  def list_songs
+    Song.all.each_with_index {|song,index|puts "#{index+1}. #{song.artist.name} - #{song.name} - #{song.genre.name}"}
+  end
+  def list_artists
+    Artist.all.each {|artist| puts artist.name}
+  end
+  def list_genres
+    Genre.all.each {|genre| puts genre.name}
+  end
+  def play_song
+    song = Song.all[gets.to_i - 1]
+    puts "Playing #{song.artist.name} - #{song.name} - #{song.genre.name}"
+  end
+  def list_artist
+    puts "Enter artist"
+    specific_artist = gets.chomp
+    if Artist.find_by_name(specific_artist) != nil
+      Artist.find_by_name(specific_artist).songs.each {|song| puts "#{song.artist.name} - #{song.name} - #{song.genre.name}"}
+    else
+      puts "Artist does not exist"
+    end
+  end
+  def list_genre
+    puts "Enter genre"
+    specific_genre = gets.chomp
+    if Genre.find_by_name(specific_genre) != nil
+      Genre.find_by_name(specific_genre).songs.each {|song| puts "#{song.artist.name} - #{song.name} - #{song.genre.name}"}
+    else
+      puts "Genre does not exist"
+    end
+  end
+end
+
 describe "MusicLibraryController" do
   describe "#initialize" do
     it "accepts one argument, the path to the MP3 files to be imported" do
